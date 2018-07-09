d4df91797bdd9ddb103ee0698411eff13cc54a9d
Sadique Ali Koothumadan <sadiqalikm@gmail.com>
2018-07-08 23:32:18 -0700

    First steps of http server

--- server/todo-server/server.go
+++ server/todo-server/server.go
@@ -1,16 +1,38 @@
 package main
 
 import (
-	"fmt"
-	"html"
+	"encoding/json"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/sdqali/todo"
 )
 
+const filePath string = "/tmp/todo.json"
+
 func main() {
-	http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	store := todo.NewJsonFileStore(filePath)
+	repo := todo.NewTodoRepo(&store)
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(repo.All())
 	})
 
+	router.HandleFunc("/{id}", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		vars := mux.Vars(r)
+		item, err := repo.Get(vars["id"])
+		if err == nil {
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(item)
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+		}
+	})
+
+	http.Handle("/", router)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
