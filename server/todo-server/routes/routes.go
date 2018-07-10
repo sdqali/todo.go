6c33171fe3bd65362831a68a5c66fa74febba534
Sadique Ali Koothumadan <sadiqalikm@gmail.com>
2018-07-09 20:47:23 -0700

    Move outes to package.

---
+++ server/todo-server/routes/routes.go
@@ -0,0 +1,28 @@
+package routes
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/sdqali/todo"
+)
+
+func List(repo todo.TodoRepo) func(writer http.ResponseWriter, request *http.Request) {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		json.NewEncoder(writer).Encode(repo.All())
+	}
+}
+
+func Get(repo todo.TodoRepo) func(writer http.ResponseWriter, request *http.Request) {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		vars := mux.Vars(request)
+		item, err := repo.Get(vars["id"])
+		if err == nil {
+			writer.WriteHeader(http.StatusOK)
+			json.NewEncoder(writer).Encode(item)
+		} else {
+			writer.WriteHeader(http.StatusNotFound)
+		}
+	}
+}
--- server/todo-server/server.go
+++ server/todo-server/server.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 	"github.com/sdqali/todo"
 	"github.com/sdqali/todo/server/todo-server/middleware"
+	"github.com/sdqali/todo/server/todo-server/routes"
 )
 
 const filePath string = "/tmp/todo.json"
@@ -17,27 +17,8 @@ func main() {
 	repo := todo.NewTodoRepo(&store)
 	router := mux.NewRouter()
 
-	router.HandleFunc("/", List(repo)).Methods("GET")
-	router.HandleFunc("/{id}", Get(repo)).Methods("GET")
+	router.HandleFunc("/", routes.List(repo)).Methods("GET")
+	router.HandleFunc("/{id}", routes.Get(repo)).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":8090", middleware.WithMiddleWares(router)))
-}
-
-func List(repo todo.TodoRepo) func(writer http.ResponseWriter, request *http.Request) {
-	return func(writer http.ResponseWriter, request *http.Request) {
-		json.NewEncoder(writer).Encode(repo.All())
-	}
-}
-
-func Get(repo todo.TodoRepo) func(writer http.ResponseWriter, request *http.Request) {
-	return func(writer http.ResponseWriter, request *http.Request) {
-		vars := mux.Vars(request)
-		item, err := repo.Get(vars["id"])
-		if err == nil {
-			writer.WriteHeader(http.StatusOK)
-			json.NewEncoder(writer).Encode(item)
-		} else {
-			writer.WriteHeader(http.StatusNotFound)
-		}
-	}
 }
