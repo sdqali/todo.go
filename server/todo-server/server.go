4bcdc470c45b5aa012620f73d46f1bd0c8f6e774
Sadique Ali Koothumadan <sadiqalikm@gmail.com>
2018-07-09 20:31:16 -0700

    Handle CORS

--- server/todo-server/server.go
+++ server/todo-server/server.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/sdqali/todo"
 )
@@ -19,28 +21,39 @@ func main() {
 	router.HandleFunc("/", List(repo)).Methods("GET")
 
 	router.HandleFunc("/{id}", Get(repo)).Methods("GET")
-
-	http.Handle("/", router)
-	log.Fatal(http.ListenAndServe(":8090", nil))
+	log.Fatal(http.ListenAndServe(":8090", JsonMiddleWare(WithCors(router))))
 }
 
-func List(repo todo.TodoRepo) func(w http.ResponseWriter, r *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(repo.All())
+func JsonMiddleWare(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		writer.Header().Set("Content-Type", "application/json")
+		handler.ServeHTTP(writer, request)
+	})
+}
+
+func WithCors(router *mux.Router) http.Handler {
+	headersOk := handlers.AllowedHeaders([]string{"Content-Type"})
+	originsOk := handlers.AllowedOrigins([]string{os.Getenv("CORS_ALLOWED_ORIGINS")})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
+
+	return handlers.CORS(originsOk, headersOk, methodsOk)(router)
+}
+
+func List(repo todo.TodoRepo) func(writer http.ResponseWriter, request *http.Request) {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		json.NewEncoder(writer).Encode(repo.All())
 	}
 }
 
-func Get(repo todo.TodoRepo) func(w http.ResponseWriter, r *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		vars := mux.Vars(r)
+func Get(repo todo.TodoRepo) func(writer http.ResponseWriter, request *http.Request) {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		vars := mux.Vars(request)
 		item, err := repo.Get(vars["id"])
 		if err == nil {
-			w.WriteHeader(http.StatusOK)
-			json.NewEncoder(w).Encode(item)
+			writer.WriteHeader(http.StatusOK)
+			json.NewEncoder(writer).Encode(item)
 		} else {
-			w.WriteHeader(http.StatusNotFound)
+			writer.WriteHeader(http.StatusNotFound)
 		}
 	}
 }
