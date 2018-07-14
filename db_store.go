3f5b17111e57de6893522b2a1d72e85271c4694c
Sadique Ali Koothumadan <sadiqalikm@gmail.com>
2018-07-13 23:57:30 -0700

    Close connections

--- db_store.go
+++ db_store.go
@@ -8,7 +8,7 @@ import (
 )
 
 const SELECT_ALL_QUERY = "SELECT id, title, item_order, completed FROM todo_items;"
-const SELECT_QUERY = "SELECT id, title, item_order, completed FROM todo_items WHERE id=$1;"
+const SELECT_QUERY = "SELECT id, title, item_order, completed FROM todo_items WHERE id=$1 LIMIT 1;"
 const INSERT_QUERY = "INSERT INTO todo_items(id, title, item_order, completed) VALUES($1, $2, $3, $4);"
 const DELETE_QUERY = "DELETE FROM todo_items WHERE id=$1;"
 const UPDATE_QUERY = "UPDATE todo_items SET title=$1, item_order=$2, completed=$3 WHERE id=$4;"
@@ -23,11 +23,17 @@ func NewDbStore(db *sql.DB) *DbStore {
 }
 
 func (store *DbStore) Add(item TodoItem) {
-	store.db.QueryRow(INSERT_QUERY, item.Id, item.Title, item.Order, item.Done)
+	rows, _ := store.db.Query(INSERT_QUERY, item.Id, item.Title, item.Order, item.Done)
+	rows.Close()
 }
 
 func (store DbStore) Get(id string) (TodoItem, error) {
-	rows, _ := store.db.Query(SELECT_QUERY, id)
+	rows, err := store.db.Query(SELECT_QUERY, id)
+	defer rows.Close()
+	if err != nil {
+		fmt.Println("ERROR: ", err)
+		return TodoItem{}, TodoItemNotFound{possibleId: id}
+	}
 	list := itemsFromRows(rows)
 	if len(list) == 0 {
 		return TodoItem{}, TodoItemNotFound{possibleId: id}
@@ -37,29 +43,37 @@ func (store DbStore) Get(id string) (TodoItem, error) {
 }
 
 func (store *DbStore) Remove(id string) {
-	store.db.Query(DELETE_QUERY, id)
+	rows, err := store.db.Query(DELETE_QUERY, id)
+	defer rows.Close()
+	if err != nil {
+		fmt.Println("ERROR: ", err)
+	}
 }
 
 func (store DbStore) All() []TodoItem {
 	rows, err := store.db.Query(SELECT_ALL_QUERY)
-
+	defer rows.Close()
 	if err == nil {
 		return itemsFromRows(rows)
 	} else {
+		fmt.Println("ERROR: ", err)
 		return make([]TodoItem, 0)
 	}
 }
 
 func (store *DbStore) Save(itemToSave TodoItem) {
-	store.db.Query(UPDATE_QUERY, itemToSave.Title, itemToSave.Order, itemToSave.Done, itemToSave.Id)
+	rows, _ := store.db.Query(UPDATE_QUERY, itemToSave.Title, itemToSave.Order, itemToSave.Done, itemToSave.Id)
+	rows.Close()
 }
 
 func (store DbStore) Find(searchTerm string) []TodoItem {
 	query := fmt.Sprintf(SEARCH_QUERY, searchTerm)
 	rows, err := store.db.Query(query)
+	defer rows.Close()
 	if err == nil {
 		return itemsFromRows(rows)
 	} else {
+		fmt.Println("ERROR: ", err)
 		return make([]TodoItem, 0)
 	}
 }
