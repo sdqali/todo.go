61e25003514527726f11e8aa9ff9a661bed1a796
Sadique Ali Koothumadan <sadiqalikm@gmail.com>
2018-07-13 21:39:03 -0700

    Remove item

--- db_store.go
+++ db_store.go
@@ -9,6 +9,7 @@ import (
 const SELECT_ALL_QUERY = "SELECT id, title, item_order, completed FROM todo_items;"
 const SELECT_QUERY = "SELECT id, title, item_order, completed FROM todo_items WHERE id=$1;"
 const INSERT_QUERY = "INSERT INTO todo_items(id, title, item_order, completed) VALUES($1, $2, $3, $4);"
+const DELETE_QUERY = "DELETE FROM todo_items WHERE id=$1;"
 
 type DbStore struct {
 	db *sql.DB
@@ -24,7 +25,7 @@ func (store *DbStore) Add(item TodoItem) {
 
 func (store DbStore) Get(id string) (TodoItem, error) {
 	rows, _ := store.db.Query(SELECT_QUERY, id)
-	list := listFromRows(rows)
+	list := itemsFromRows(rows)
 	if len(list) == 0 {
 		return TodoItem{}, TodoItemNotFound{possibleId: id}
 	} else {
@@ -32,7 +33,24 @@ func (store DbStore) Get(id string) (TodoItem, error) {
 	}
 }
 
-func listFromRows(rows *sql.Rows) []TodoItem {
+func (store *DbStore) Remove(id string) {
+	store.db.Query(DELETE_QUERY, id)
+}
+
+func (store DbStore) All() []TodoItem {
+	rows, _ := store.db.Query(SELECT_ALL_QUERY)
+	return itemsFromRows(rows)
+}
+
+func (store *DbStore) Save(itemToSave TodoItem) {
+}
+
+func (store DbStore) Find(searchTerm string) []TodoItem {
+	var list []TodoItem
+	return list
+}
+
+func itemsFromRows(rows *sql.Rows) []TodoItem {
 	var list []TodoItem
 	for rows.Next() {
 		var id uuid.UUID
@@ -43,21 +61,5 @@ func listFromRows(rows *sql.Rows) []TodoItem {
 		rows.Scan(&id, &title, &order, &completed)
 		list = append(list, TodoItem{Id: id, Title: title, Order: order, Done: completed})
 	}
-	return list
-}
-
-func (store *DbStore) Remove(id string) {
-}
-
-func (store DbStore) All() []TodoItem {
-	rows, _ := store.db.Query(SELECT_ALL_QUERY)
-	return listFromRows(rows)
-}
-
-func (store *DbStore) Save(itemToSave TodoItem) {
-}
-
-func (store DbStore) Find(searchTerm string) []TodoItem {
-	var list []TodoItem
 	return list
 }
